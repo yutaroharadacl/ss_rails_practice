fec437b8a7ceedeac2d872ce8845d380fa815644
yutaroharadacl <yutaro.harada.cl@gmail.com>
2026-09-03 11:18:04 +0900

    feat: 合計金額の計算機能と購入ボタンを追加

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe Cart, type: :model do
       expect { cart.destroy! }.to change(CartItem, :count).by(-1)
     end
   end
+
+  describe 'total_price' do
+    it 'cart_itemが1件、数量も1時、計算結果が正しいこと' do
+      cart = Cart.create!
+      cart.cart_items.create!(product_id: 1, quantity: 1)
+      expect(cart.total_price).to eq(BigDecimal('1100'))
+    end
+
+    it 'cart_itemが1件、数量が2の時、計算結果が正しいこと' do
+      cart = Cart.create!
+      cart.cart_items.create!(product_id: 1, quantity: 2)
+      expect(cart.total_price).to eq(BigDecimal('2200'))
+    end
+
+    it 'cart_itemが2件の時、計算結果が正しいこと' do
+      cart = Cart.create!
+      cart.cart_items.create!(product_id: 1, quantity: 2)
+      cart.cart_items.create!(product_id: 2, quantity: 1)
+      expect(cart.total_price).to eq(BigDecimal('3300'))
+    end
+  end
 end

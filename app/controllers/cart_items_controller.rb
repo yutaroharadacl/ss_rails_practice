91bee2955d41728c60c9d94ea3e538dd5c521919
yutaroharadacl <yutaro.harada.cl@gmail.com>
2026-09-04 17:16:38 +0900

    PR #5レビュー指摘の対応 (#7)
    * fix: レビュー指摘対応
    
    * fix: テスト修正
    
    * fix: aiレビュー指摘対応

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 class CartItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound do |_e|
+    redirect_to cart_path, alert: t('flash.cart_items.error.not_found')
+  end
+
   def create
     # 現在のカートにすでに対象の商品が登録されていれば数量を増加させる。それ以外の場合は新規作成
     # find_or_initialize_byは存在していればそれをitemに格納、存在していなければ作成(new)してitemに格納してくれる
     item = current_cart.cart_items.find_or_initialize_by(product_id: cart_item_params[:product_id])
     # アイテムの数量を送られてきたquantity分増加、to_iは数値にするという意味
     item.quantity += cart_item_params[:quantity].to_i
-    redirect_after_save(item, notice: 'カートに追加しました')
+    redirect_after_save(item, notice: t('flash.cart_items.create.notice'))
   end
 
   def update
@@ -21,7 +25,7 @@ class CartItemsController < ApplicationController
   def destroy
     item = current_cart.cart_items.find(params[:id])
     item.destroy!
-    redirect_back fallback_location: cart_path
+    redirect_to cart_path
   end
 
   private
@@ -34,9 +38,9 @@ class CartItemsController < ApplicationController
   def redirect_after_save(item, notice: nil)
     if item.save
       # 保存に成功したら画面は元の画面になる。失敗した時にはカート画面へ遷移
-      redirect_back fallback_location: cart_path, notice: notice
+      redirect_to cart_path, notice: notice
     else
-      redirect_back fallback_location: cart_path, alert: item.errors.full_messages.join(', ')
+      redirect_to cart_path, alert: item.errors.full_messages.join(', ')
     end
   end
 end

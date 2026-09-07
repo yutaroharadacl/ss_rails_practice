4a0f9785a6e0177ff8cbb7b6b6184f4479c0bef4
Takuya Okamoto <taku@TakuyanoMacBook-Pro.local>
2026-09-07 16:32:41 +0900

    検索機能とページネーション、テストコードを実装

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,63 +1,92 @@
-class Admin::ProductsController < ApplicationController
-  before_action :set_store
-  before_action :set_product, only: %i[show edit update destroy]
+# frozen_string_literal: true
 
+module Admin
+  class ProductsController < ApplicationController
+    before_action :set_store
+    before_action :set_product, only: %i[show edit update destroy]
 
-  def index
-    @products = @store.products.includes(:sku)
-  end
+    def index
+      @q_name = params[:name].to_s.strip
+      @q_sku_code = params[:sku_code].to_s.strip
+      @q_published = params[:published].presence || 'all'
+      @q_price_min = params[:price_min].to_s.strip
+      @q_price_max = params[:price_max].to_s.strip
 
-  def show
-    @product = @store.products.find(params[:id])
+      @searched = @q_name.present? || @q_sku_code.present? ||  # ユーザー側のみ
+                  @q_price_min.present? ||
+                  @q_price_max.present? ||
+                  @q_published != 'all'
 
-  end
+      if @searched
+        products = @store.products.includes(:sku)
+        products = products.where('products.name LIKE ?', "%#{Product.sanitize_sql_like(@q_name)}%") if @q_name.present?
+        products = products.where(published: true) if @q_published == 'true'
+        products = products.where(published: false) if @q_published == 'false'
+        if @q_sku_code.present? || @q_price_min.present? || @q_price_max.present?
+          products = products.joins(:sku)
+          if @q_sku_code.present?
+            products = products.where('skus.code LIKE ?',
+                                      "%#{Sku.sanitize_sql_like(@q_sku_code)}%")
+          end
+          products = products.where('skus.price >= ?', @q_price_min.to_i) if @q_price_min.present?
+          products = products.where('skus.price <= ?', @q_price_max.to_i) if @q_price_max.present?
+        end
+        @products = products
+      else
+        @products = Product.none
+      end
+    end
+    
+    def show
+      @product = @store.products.find(params[:id])
+    end
 
-  def new
-    @product = @store.products.build
-    @product.build_sku
-  end
+    def new
+      @product = @store.products.build
+      @product.build_sku
+    end
 
-  def create
-    @product = @store.products.build(product_params)
-    if @product.save
-      redirect_to admin_store_product_path(@store, @product)
-    else
-      render :new
+    def create
+      @product = @store.products.build(product_params)
+      if @product.save
+        redirect_to admin_store_product_path(@store, @product)
+      else
+        render :new
+      end
+    end
+
+    def edit; end
+
+    def update
+      if @product.update(product_params)
+        redirect_to admin_store_product_path(@store, @product)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @product.destroy
+      redirect_to admin_store_products_path(@store)
+    end
+
+    private
+
+    def set_store
+      @store = Store.find(params[:store_id])
+    end
+
+    def set_product
+      @product = @store.products.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(
+        :name,
+        :description,
+        :published,
+        sku_attributes: %i[id code price stock_quantity]
+      )
     end
   end
-
-  def edit
-  end
-
-  def update
-    if @product.update(product_params)
-      redirect_to admin_store_product_path(@store, @product)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @product.destroy
-    redirect_to admin_store_products_path(@store)
-  end
-
-  private
-  def set_store
-    @store = Store.find(params[:store_id])
-  end
-
-  def set_product
-    @product = @store.products.find(params[:id])
-  end
-
-  def product_params
-    params.require(:product).permit(
-      :name,
-      :description,
-      :published,
-      sku_attributes: %i[id code price stock_quantity]
-    )
-  end
-
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,39 @@
 class ProductsController < ApplicationController
+  PER_PAGE = 20
+
   def index
-    @products = Product.where(published: true).includes(:sku, :store)
+    @q_name = params[:name].to_s.strip
+    @q_sku_code = params[:sku_code].to_s.strip
+    @q_store_name = params[:store_name].to_s.strip
+    @q_price_min = params[:price_min].to_s.strip
+    @q_price_max = params[:price_max].to_s.strip
+
+    # 検索フォーム送信（またはページネーションで search=1 が残っている）かどうか
+    @submitted = params[:search].present?
+
+    if @submitted
+      scope = Product.where(published: true).includes(:sku, :store)
+
+      scope = scope.where('products.name LIKE ?', "%#{Product.sanitize_sql_like(@q_name)}%") if @q_name.present?
+
+      if @q_store_name.present?
+        scope = scope.joins(:store).where('stores.name LIKE ?', "%#{Store.sanitize_sql_like(@q_store_name)}%")
+      end
+
+      if @q_sku_code.present? || @q_price_min.present? || @q_price_max.present?
+        scope = scope.joins(:sku)
+        scope = scope.where('skus.code LIKE ?', "%#{Sku.sanitize_sql_like(@q_sku_code)}%") if @q_sku_code.present?
+        scope = scope.where('skus.price >= ?', @q_price_min.to_i) if @q_price_min.present?
+        scope = scope.where('skus.price <= ?', @q_price_max.to_i) if @q_price_max.present?
+      end
+    else
+      scope = Product.none
+    end
+
+    @page = [params[:page].to_i, 1].max
+    @total_count = scope.count
+    @total_pages = [(@total_count.to_f / PER_PAGE).ceil, 1].max
+    @page = @total_pages if @page > @total_pages
+    @products = scope.order(:id).limit(PER_PAGE).offset((@page - 1) * PER_PAGE)
   end
 end
